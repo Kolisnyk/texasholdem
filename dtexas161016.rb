9eb3e1d723500236e8b47dafc68f075ab2ee6276
Kolisnyk <Kolisnyk@inbox.ru>
2016-10-23 23:04:32 +0300

    changed according to issues

--- dtexas161016.rb
+++ dtexas161016.rb
@@ -18,9 +18,7 @@
   #massive for conversion to cards' names
   hash_massive = {1 => "2 heart", 2 => "3 heart", 3 => "4 heart", 4 => "5 heart", 5 => "6 heart", 6 => "7 heart", 7 => "8 heart", 8 => "9 heart", 9 => "10 heart", 10 => "jack heart", 11 => "queen heart", 12 => "king heart", 13 => "ace heart", 14 => "2 diamond", 15 => "3 diamond", 16 => "4 diamond", 17 => "5 diamond", 18 => "6 diamond", 19 => "7 diamond", 20 => "8 diamond", 21 => "9 diamond", 22 => "10 diamond", 23 => "jack diamond", 24 => "queen diamond", 25 => "king diamond", 26 => "ace diamond", 27 => "2 club", 28 => "3 club", 29 => "4 club", 30 => "5 club", 31 => "6 club", 32 => "7 club", 33 => "8 club", 34 => "9 club", 35 => "10 club", 36 => "jack club", 37 => "queen club", 38 => "king club", 39 => "ace club", 40 => "2 spade", 41 => "3 spade", 42 => "4 spade", 43 => "5 spade", 44 => "6 spade", 45 => "7 spade", 46 => "8 spade", 47 => "9 spade", 48 => "10 spade", 49 => "jack spade", 50 => "queen spade", 51 => "king spade", 52 => "ace spade"}
   #generate the cards on the table
-  cards = []
   cards = a.shuffle.first(7)
-  cards.shuffle!
 
   ##! methods for use
   #method for the conversion of numbers to cards from hash - работает,написанному верить.
@@ -75,10 +73,8 @@
     if marker_of_win != 1
       for j in 0..3
         if ((a1[j])==(a1[j+1]-13)&&((a1[j+1])==(a1[j+2]-13)) && ((a1[j+2]) == (a1[j+3]-13)))
-          if ((a1[i]==hands[0]) || (a1[i]==hands[1]))
-            win_combination = "isQuads"
-            marker_of_win = 1
-          end
+          win_combination = "isQuads"
+          marker_of_win = 1
         end
       end
     end
@@ -89,10 +85,8 @@
           for i in 0..5
             if (a1[i] == (a1[i+1]-13))
               if (i!=j)
-                if ((a1[i]==hands[0]) || (a1[i]==hands[1]))
-                  win_combination = "isFullHouse"
-                  marker_of_win = 1
-                end
+                win_combination = "isFullHouse"
+                marker_of_win = 1
               end
             end
           end
@@ -116,11 +110,8 @@
       temp = temp.sort {|x,y| x<=>y}
       for i in 0..2
         if (temp[i] == temp[i+1]-1) && (temp[i+1] == temp[i+2]-1) && (temp[i+2] == temp[i+3]-1) && (temp[i+3] == temp[i+4]-1)
-          #checking if cards in your hands present in win combination
-          if ((temp[i]==hands[0]) || (temp[i+1]==hands[0]) || (temp[i+2]==hands[0]) || (temp[i+3]==hands[0]) || (temp[i+4]==hands[0]) || (temp[i]==hands[1]) || (temp[i+1]==hands[1]) || (temp[i+2]==hands[1]) || (temp[i+3]==hands[1]) || (temp[i+4]==hands[1]))
-            win_combination = "isStraight"
-            marker_of_win = 1
-          end
+          win_combination = "isStraight"
+          marker_of_win = 1
         end
       end
     end
