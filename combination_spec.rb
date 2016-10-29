2e21d4090693a273eaffcb794046fb655064f034
Kolisnyk <Kolisnyk@inbox.ru>
2016-10-29 13:22:36 +0300

    done with some Rubocops propositions disagree

--- combination_spec.rb
+++ combination_spec.rb
@@ -5,56 +5,57 @@ require_relative 'combination.rb'
 describe 'combination' do
   context 'RoyalFlush' do
     it 'should do 1' do
-      expect( Combination.royal_flush([1, 14, 48, 49, 50, 51, 52], 0)).to eq(1)
+      expect(Combination.royal_flush([1, 14, 48, 49, 50, 51, 52], 0)).to
+      eq(1)
     end
   end
 
   context 'StraightFlush' do
     it 'should do 1' do
-      expect( Combination.straight_flush([1, 14, 47, 48, 49, 50, 51], 0)).to eq(1)
+      expect(Combination.straight_flush([1, 14, 47, 48, 49, 50, 51], 0)).to
+      eq(1)
     end
   end
 
   context 'Quads' do
     it 'should do 1' do
-      expect( Combination.quads([1, 14, 27, 40, 41, 43, 45], 0)).to eq(1)
+      expect(Combination.quads([1, 14, 27, 40, 41, 43, 45], 0)).to eq(1)
     end
   end
 
   context 'FullHouse' do
     it 'should do 1' do
-      expect( Combination.full_house([1, 14, 27, 28, 41, 43, 44], 0)).to eq(1)
+      expect(Combination.full_house([1, 14, 27, 28, 41, 43, 44], 0)).to eq(1)
     end
   end
 
   context 'Straight' do
     it 'should do 1' do
-      expect( Combination.straight([1, 15, 29, 43, 44, 50, 51], 0)).to eq(1)
+      expect(Combination.straight([1, 15, 29, 43, 44, 50, 51], 0)).to eq(1)
     end
   end
 
   context 'Flush' do
     it 'should do 1' do
-      expect( Combination.flush([1, 3, 5, 7, 9, 32, 45], 0)).to eq(1)
+      expect(Combination.flush([1, 3, 5, 7, 9, 32, 45], 0)).to eq(1)
     end
   end
 
   context 'Set' do
     it 'should do 1' do
-      expect( Combination.set([1, 14, 27, 39, 41, 43, 45], 0)).to eq(1)
+      expect(Combination.set([1, 14, 27, 39, 41, 43, 45], 0)).to eq(1)
     end
   end
 
-   context 'TwoPairs' do
-     it 'should do 1' do
-       expect( Combination.two_pair([1, 14, 28, 30, 41, 48, 50], 0)).to eq(1)
-     end
-   end
+  context 'TwoPairs' do
+    it 'should do 1' do
+      expect(Combination.two_pair([1, 14, 28, 30, 41, 48, 50], 0)).to eq(1)
+    end
+  end
 
-   context 'OnePair' do
-     it 'should do 1' do
-       expect( Combination.one_pair([1, 14, 23, 30, 41, 48, 51], 0)).to eq(1)
-     end
-   end
-
+  context 'OnePair' do
+    it 'should do 1' do
+      expect(Combination.one_pair([1, 14, 23, 30, 41, 48, 51], 0)).to eq(1)
+    end
+  end
 end
